d28c657a0f6245e438f47aec11ff82b6849270c5
Unknown <nemirovsky.nirel@gmail.com>
2017-04-02 20:19:43 +0300

    modified shit

---
+++ db/databaseHandler.js
@@ -0,0 +1,175 @@
+let handler = {};
+let Sequelize = require('sequelize');
+let DataTypes = require('sequelize/lib/data-types');
+const Promise = require('promise');
+
+  handler.create = function(){
+    return new Promise(function(fulfill, reject){
+      let sequelize = new Sequelize('ctform', 'root', 'mysqlzhd123Q!@#', {
+        host: '162.243.8.121',
+        dialect: 'mysql',
+        pool: {
+          max: 50,
+          min: 1,
+          idle: 10000
+        }
+      });
+      if(sequelize){
+        fulfill(sequelize);
+      }else{
+        reject();
+      }
+    });
+  }
+
+  // handler.createCampaignsModel = function(){
+  //     return new Promise(function(fulfill, reject){
+  //       handler.create().then(function(sequelize){
+  //         console.log('created sequelize');
+  //         // sequelize = sequelize;
+  //
+  //         var CampaignModel = sequelize.define('campaignsModel', {
+  //           title: {
+  //             type: DataTypes.STRING
+  //           },
+  //           sites: {
+  //             type: DataTypes.STRING
+  //           },
+  //           firstName: {
+  //             type: DataTypes.STRING
+  //           },
+  //           lastName: {
+  //             type: DataTypes.STRING
+  //           },
+  //           email: {
+  //             type: DataTypes.STRING
+  //           },
+  //           phoneNumber: {
+  //             type: DataTypes.STRING
+  //           },
+  //           company: {
+  //             type: DataTypes.STRING
+  //           },
+  //           address: {
+  //             type: DataTypes.STRING
+  //           },
+  //           city: {
+  //             type: DataTypes.STRING
+  //           },
+  //           url: {
+  //             type: DataTypes.STRING
+  //           },
+  //           job: {
+  //             type: DataTypes.STRING
+  //           },
+  //           messageTitle: {
+  //             type: DataTypes.STRING
+  //           },
+  //           inquiry: {
+  //             type: DataTypes.STRING
+  //           }
+  //         }, {
+  //           freezeTableName: true // Model tableName will be the same as the model name
+  //         });
+  //
+  //       var SearchWordsModel = sequelize.define('searchWordsModel', {
+  //             word: {
+  //                 type: DataTypes.STRING
+  //             },
+  //             campaignsModelId: {
+  //                 type: DataTypes.INTEGER
+  //             }
+  //           }, {
+  //               freezeTableName: true // Model tableName will be the same as the model name
+  //           });
+  //
+  //         sequelize.sync().then(function() {
+  //           if(CampaignModel && SearchWordsModel){
+  //             console.log('campaignModel and SearchWordsModel created!');
+  //             CampaignModel.hasMany( SearchWordsModel, {as:'searchWords'});
+  //             SearchWordsModel.hasOne( CampaignModel, {as:'campaignModel'});
+  //
+  //             fulfill({CampaignModel:CampaignModel, SearchWordsModel:SearchWordsModel});
+  //           }else {
+  //             reject('couldnt create campaign & searchWords model');
+  //           }
+  //         });
+  //       });
+  //     });
+  //   }
+
+  handler.createBlackListModel = function(){
+    var BlackListModel = sequelize.define('blackListModel', {
+      siteLink: {
+        type: DataTypes.STRING,
+      }
+    }, {
+      freezeTableName: true // Model tableName will be the same as the model name
+    });
+
+    return BlackListModel;
+  }
+
+  // handler.addNewCampaign = function(CampaignModel ,data){
+  //   return new Promise(function(fulfill, reject){
+  //     if(CampaignModel){
+  //       CampaignModel
+  //         .create({
+  //           title: data.title,
+  //           sites: data.sites,
+  //           firstName: data.firstName,
+  //           lastName: data.lastName,
+  //           email: data.email,
+  //           phoneNumber: data.phoneNumber,
+  //           company: data.company,
+  //           address: data.address,
+  //           city: data.city,
+  //           url: data.url,
+  //           job: data.job,
+  //           messageTitle: data.messageTitle,
+  //           inquiry: data.inquiry
+  //         })
+  //         .then(function(newCampaign) {
+  //           fulfill(newCampaign);
+  //         });
+  //     }else {
+  //       console.log('campaign model was not created');
+  //     }
+  //   });
+  // }
+  
+  // handler.addSearchWordsToCampaign = function (searchWordsModel, campaignInstance, searchWords) {
+  //     return new Promise(function(fulfill, reject) {
+  //         let searchDataArray = [];
+  //         searchWords.forEach(function (word) {
+  //             searchDataArray.push({
+  //                 word: word
+  //             });
+  //         });
+  //
+  //         // searchWordsModel.create({word:searchWords[0]}).then(function (instance) {
+  //         //   console.log(instance);
+  //         //     campaignInstance.setSearchWords([instance]).then(function (temp) {
+  //         //                 console.log(temp);
+  //         //             })
+  //         //             .catch(function(err){
+  //         //                 if(err){
+  //         //                   console.log(err);
+  //         //                 }
+  //         //             });
+  //         // });
+  //         searchWordsModel.bulkCreate(searchDataArray).then(function (searchWordsCreated) {
+  //             campaignInstance.setSearchWords(searchWordsCreated).then(function (temp) {
+  //                 console.log(temp);
+  //                 fulfill(temp);
+  //             })
+  //             .catch(function(err){
+  //                 if(err){
+  //                   console.log(err);
+  //                 }
+  //             });
+  //         });
+  //     });
+  // };
+
+module.exports = handler;
--- rest/express.js
+++ rest/express.js
@@ -1,20 +1,33 @@
 var express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser');
-
+var CampaignsDb = require('../db/Campaigns');
+let database = require('../db/databaseHandler');
 var app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var handler = {};
-
+var db = new CampaignsDb();
 app.get('/', function(req, res){
-  res.send('server is here');
+  database.create();
 });
 
 app.post('/newProject', function(req, res){
-  res.send(`new project here - ${req.body.projectName}`);
+  database.createCampaignsModel().then(function(models){
+      console.log('model created, calling newCampaign');
+      var campaignModel = models.CampaignModel, searchWordsModel = models.SearchWordsModel;
+      database.addNewCampaign(campaignModel, req.body.data).then(function(campaign){
+        console.log('new campaign created');
+        // calling search words creator
+        database.addSearchWordsToCampaign(searchWordsModel, campaign,req.body.data.searchWords)
+            .then(function (searchWordsModel) {
+                console.log('search words models created!!');
+            });
+        res.send('ok');
+      });
+  });
 });
 
 handler.listen = function(){
