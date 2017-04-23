bb779cafb4211574d56fd3eb4d84908ad656d136
yiftach edelstain <yiftach.edelstain@gmail.com>
2017-04-23 23:06:11 +0300

    deploy possible fix

--- GoogleSearcher.js
+++ GoogleSearcher.js
@@ -35,99 +35,102 @@ searcher.search = function (campaign, limit) {
 };
 
 const searchKeyword = function (keyword, limit, campaign, report) {
-    console.log('searching keyword ' + keyword);
-    return new Promise(function (resolve, reject) {
-        visitor.logger = searcher.logger;
-        const chromeCapabilities = driver.Capabilities.chrome();
-        const chromeOptions = {
-            'args': ['--lang', 'en-US']
-        };
-        chromeCapabilities.set('chromeOptions', chromeOptions);
-        let browser = new Builder()
-            .withCapabilities(chromeCapabilities)
-            .build();
-        browser.manage()
-            .timeouts()
-            .pageLoadTimeout(300000000);
-        browser.get("http://www.google.com");
-        var a = browser.findElement(By.name('q')).sendKeys(keyword).then(function () {
-            browser.findElement(By.name('q')).sendKeys(driver.Key.ENTER);
-        });
-        let connection = database.create();
-        var chain = Promise.resolve(report);
-        for (let i = 0; i < (limit / 10); i++) {
-            chain = chain.then(report => {
-                return new Promise(function (resolve, reject) {
-                    findLinks(browser).then(function (links) {
-                        utils.other.promiseSerial(
-                                links.map(link => () => visitor.visitSite(connection, link, campaign)))
-                            .then(function (results) {
-                                var success = results.filter(x => x.status === "resolved");
-                                var blockedByBLNumber = results.filter(x => x.status === "rejected").filter(x => x.e === "already visited");
-                                report.sitesVisitedNumber += results.length;
-                                report.cfSentNumber += success.length;
-                                report.blockedByBLNumber += blockedByBLNumber.length;
-                                report.noCfNumber += results.length - success.length - blockedByBLNumber.length;
+console.log('searching keyword ' + keyword);
+return new Promise(function (resolve, reject) {
+    visitor.logger = searcher.logger;
+    const chromeCapabilities = driver.Capabilities.chrome();
+    const chromeOptions = {
+        'args': ['--lang', 'en-US']
+    };
+    chromeCapabilities.set('chromeOptions', chromeOptions);
+    let browser = new Builder()
+        .withCapabilities(chromeCapabilities)
+        .build();
+    browser.manage()
+        .timeouts()
+        .pageLoadTimeout(300000000);
+    browser.get("http://www.google.com");
+    var a = browser.findElement(By.name('q')).sendKeys(keyword).then(function () {
+        browser.findElement(By.name('q')).sendKeys(driver.Key.ENTER);
+    });
+    let connection = database.create();
+    var chain = Promise.resolve(report);
+    for (let i = 0; i < (limit / 10); i++) {
+        chain = chain.then(report => {
+            return new Promise(function (resolve, reject) {
+                findLinks(browser).then(function (links) {
+                    utils.other.promiseSerial(
+                            links.map(link => () => visitor.visitSite(connection, link, campaign)))
+                        .then(function (results) {
+                            var success = results.filter(x => x.status === "resolved");
+                            var blockedByBLNumber = results.filter(x => x.status === "rejected").filter(x => x.e === "already visited");
+                            report.sitesVisitedNumber += results.length;
+                            report.cfSentNumber += success.length;
+                            report.blockedByBLNumber += blockedByBLNumber.length;
+                            report.noCfNumber += results.length - success.length - blockedByBLNumber.length;
 
-                                var promise = function () {
-                                    return browser.getCurrentUrl()
-                                        .then(function (url) {
-                                            return browser.get(url)
+                            var promise = function () {
+                                return browser.getCurrentUrl()
+                                    .then(function (url) {
+                                        return browser.get(url)
+                                            .then(() =>
+                                                browser.wait(until.elementLocated(By.css("td:last-of-type.navend > a.pn")), 4000)
                                                 .then(() =>
-                                                    browser.wait(until.elementLocated(By.css("td:last-of-type.navend > a.pn")),4000)
-                                                    .then(() =>
-                                                        browser.findElement(By.css("td:last-of-type.navend > a.pn"))
-                                                    ))
-                                        })
-                                };
-                                return promise().then(function (element) {
-                                    element.click();
-                                    resolve(report);
-                                }).catch(function (err) {
-                                    reject(new utils.exceptions.NoMoreResultsException(report));
-                                });
+                                                    browser.findElement(By.css("td:last-of-type.navend > a.pn"))
+                                                ))
+                                    })
+                            };
+                            return promise().then(function (element) {
+                                element.click();
+                                resolve(report);
                             }).catch(function (err) {
-                                if (err.name && err.name === 'NoSuchElementError' && report) {
-                                    reject(new utils.exceptions.NoMoreResultsException(report));
-                                } else {
-                                    console.log(err.stack)
-                                    err.results = report
-                                    reject(err);
-                                }
+                                reject(new utils.exceptions.NoMoreResultsException(report));
                             });
-                    }).catch(function (err) {
-                        if (err === "LastPage") {
-                            reject(new utils.exceptions.NoMoreResultsException(report));
-                        } else {
-                            err.results = report;
-                            reject(err);
-                        }
-                    })
-                });
-            }).catch(function (err) {
-                console.log('found an error');
-                console.log(err);
-                if (err.constructor.name === "NoMoreResultsException") {
-                    return err.results;
-                } else {
-                    reject(report);
-                }
+                        }).catch(function (err) {
+                            if (err.name && err.name === 'NoSuchElementError' && report) {
+                                reject(new utils.exceptions.NoMoreResultsException(report));
+                            } else {
+                                console.log(err.stack)
+                                err.results = report
+                                reject(err);
+                            }
+                        });
+                }).catch(function (err) {
+                    if (err === "LastPage") {
+                        reject(new utils.exceptions.NoMoreResultsException(report));
+                    } else {
+                        err.results = report;
+                        reject(err);
+                    }
+                })
             });
-        }
-        chain.then(function (report) {
-            browser.quit();
-            self.closedState = true;
-            resolve(report)
         }).catch(function (err) {
-            if (typeof (err) === utils.exceptions.NoMoreResultsException) {
-                browser.quit();
-                self.closedState = true;
-                resolve(err.results)
+            console.log('found an error');
+            console.log(err);
+            if (err.constructor.name === "NoMoreResultsException") {
+                return err.results;
             } else {
-                reject(err);
+                reject(report);
             }
         });
+    }
+    chain.then(function (report) {
+        browser.quit().then(function () {;
+            self.closedState = true;
+            resolve(report)
+        }).catch(e => resolve(report));
     });
+}).catch(function (err) {
+    if (typeof (err) === utils.exceptions.NoMoreResultsException) {
+        browser.quit().then(function () {;
+            self.closedState = true;
+            resolve(err.results)
+        }).catch(e => resolve(err.results));
+    } else {
+        reject(err);
+    }
+});
+});
 };
 
 const findLinks = function (browser) {
